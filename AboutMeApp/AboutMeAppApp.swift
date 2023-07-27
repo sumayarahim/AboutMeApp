38588506cb0ab99e7318b9143d5987b377162ede
Sumaya Rahim <sumayalamisah@gmail.com>
2023-07-27 13:36:11 +0100

    Initial Commit

---
+++ AboutMeApp/AboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeAppApp.swift
+//  AboutMeApp
+//
+//  Created by scholar on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
